39c34524ba2a3afa02b0eb693ea4240667644e17
Bryan Lincoln <bryanufg@gmail.com>
2021-06-27 21:22:36 -0300

    feat: add nh radius experiment

--- benchmark.sh
+++ benchmark.sh
@@ -3,9 +3,6 @@
 # exit on error
 set -e
 
-# disable info logging
-export SPDLOG_LEVEL=error
-
 function run_lattice {
     # echo header
     echo "size,iterations,loadTime,totalEvolveTime,totalBufferTime,avgEvolveTime,avgBufferTime" > $2
@@ -34,14 +31,31 @@ function run_kernel_config {
     done
 }
 
-
+# disable info logging
+export SPDLOG_LEVEL=error
+# how many times to repeat the same experiment
+TRIALS=5
+# run ID
 RUN="$(ls -1v res | tail -1 | cut -d "." -f1)"
 RUN=$(($RUN + 1))
 
+
 printf "Benchmark #$RUN\n"
 
-TRIALS=5
+### KERNEL CONFIG EXPRIMENT
 
+printf "\nBenchmarking GPU for kernel config"
+for i in $(seq 1 $TRIALS); do
+    printf "\nTrial #$i\n"
+    run_kernel_config "" "res/$RUN.$i.k.hl.gpu.8704.csv"
+done
+
+### LATTICE SIZE EXPERIMENTS
+
+# single GPU thread experiment
+run_lattice "--gpu-blocks 1 --gpu-threads 1" "res/$RUN.1.hl.gpu.1.csv"
+
+# CPU threads
 for i in $(seq 1 $TRIALS); do
     printf "\nTrial #$i\n"
     for threads in {1,2,4,8,12,16}; do
@@ -52,6 +66,16 @@ for i in $(seq 1 $TRIALS); do
     done
 done
 
+# GPU
+printf "\nBenchmarking GPU"
+for i in $(seq 1 $TRIALS); do
+    printf "\nTrial #$i\n"
+    run_lattice "" "res/$RUN.$i.hl.gpu.8704.csv"
+done
+
+### RENDERING EXPERIMENTS
+
+# CPU threads
 for i in $(seq 1 $TRIALS); do
     printf "\nTrial #$i\n"
     for threads in {16,}; do # 1,2,4,8,12,
@@ -62,22 +86,37 @@ for i in $(seq 1 $TRIALS); do
     done
 done
 
-# run_lattice "--gpu-blocks 1 --gpu-threads 1" "res/$RUN.1.hl.gpu.1.csv"
-
-printf "\nBenchmarking GPU"
-for i in $(seq 1 $TRIALS); do
-    printf "\nTrial #$i\n"
-    run_lattice "" "res/$RUN.$i.hl.gpu.8704.csv"
-done
-
-printf "\nBenchmarking GPU with render"
+# GPU
+printf "\nBenchmarking GPU with render\n"
 for i in $(seq 1 $TRIALS); do
     printf "\nTrial #$i\n"
     run_lattice "-r" "res/$RUN.$i.render.gpu.8704.csv"
 done
 
-printf "\nBenchmarking GPU for kernel config"
-for i in $(seq 1 $TRIALS); do
-    printf "\nTrial #$i\n"
-    run_kernel_config "" "res/$RUN.$i.k.hl.gpu.8704.csv"
+### NEIGHBOURHOOD EXPERIMENTS
+
+printf "\nBenchmarking Neighbourhood Radius\n"
+for nh_radius in $(seq 1 5); do
+    printf "\nRadius: $nh_radius\nRecompiling...\n"
+    make clean
+    make automata NH_RADIUS=$nh_radius
+
+    for i in $(seq 1 $TRIALS); do
+        printf "\nTrial #$i\n"
+
+        filename="res/$RUN.$i.nh.$nh_radius.csv"
+        echo "hw,threads,size,iterations,loadTime,totalEvolveTime,totalBufferTime,avgEvolveTime,avgBufferTime" > $filename
+
+        for threads in {1,2,4,8,12,16}; do
+            export OMP_NUM_THREADS=$threads
+            export OMP_SCHEDULE=dynamic,1
+            printf "\nBenchmarking CPU with $OMP_NUM_THREADS threads\n"
+            echo -n "cpu,$threads,4096," >> $filename
+            ./automata -b -x 4096 -y 4096 -p 0.5 --cpu >> $filename
+        done
+
+        echo -n "gpu,8704,4096," >> $filename
+        ./automata -b -x 4096 -y 4096 -p 0.5 >> $filename
+
+    done
 done
